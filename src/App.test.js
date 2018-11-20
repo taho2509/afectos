9bc1813a00f9e4cf160f6ad893e0fa153ca3b351
Angel Ulise Tabares Gonzalez <ulise@uci.cu>
2018-11-20 11:22:11 -0500

    [ADD] - Adding test suite biolerplate

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import AfectosForm from './components/AfectosForm';
+import Enzyme, {mount} from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+Enzyme.configure({ adapter: new Adapter() });
+
+describe("AfectosForm", () => {
+    let mountedAfectosForm;
+    const afectosForm = () => {
+        if (!mountedAfectosForm) {
+            mountedAfectosForm = mount(
+                <AfectosForm/>
+            );
+        }
+        return mountedAfectosForm;
+    };
+
+    beforeEach(() => {
+        mountedAfectosForm = undefined;
+    });
+
+    // All tests will go here
+    it("always renders a div", () => {
+        const divs = afectosForm().find("form");
+        expect(divs.length).toBeGreaterThan(0);
+    });
 });
