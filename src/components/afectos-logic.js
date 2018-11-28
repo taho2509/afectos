78dbb24118a03afd59cb9a9b3df8a299499a297c
Angel Ulise Tabares Gonzalez <taho2509@gmail.com>
2018-11-28 11:39:45 -0500

    [FIX] - fixing month indeces used to build the string date

--- src/components/afectos-logic.js
+++ src/components/afectos-logic.js
@@ -89,18 +89,18 @@ export function afectos(nameString, dateString) {
 
 export function generateStringDate(argDate) {
     const esMonth = {
-        1: "enero",
-        2: "febrero",
-        3: "marzo",
-        4: "abril",
-        5: "mayo",
-        6: "junio",
-        7: "julio",
-        8: "agosto",
-        9: "septiembre",
-        10: "octubre",
-        11: "noviembre",
-        12: "diciembre"
+        0: "enero",
+        1: "febrero",
+        2: "marzo",
+        3: "abril",
+        4: "mayo",
+        5: "junio",
+        6: "julio",
+        7: "agosto",
+        8: "septiembre",
+        9: "octubre",
+        10: "noviembre",
+        11: "diciembre"
     };
 
     const esWeekDay = {
