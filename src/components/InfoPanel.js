187e2678cabb3abf733f40f5f3d97423e5f67518
Angel Ulise Tabares Gonzalez <taho2509@gmail.com>
2018-11-29 11:31:40 -0500

    [ADD] - mostrando imagenes por cada afecto en lugar de solo texto

--- src/components/InfoPanel.js
+++ src/components/InfoPanel.js
@@ -1,27 +1,54 @@
 import React, {Component} from "react";
-import {Button, Panel} from "react-bootstrap";
-import * as PropTypes from "prop-types";
+import {Button, Grid, Row, Col, Panel} from "react-bootstrap";
 
 export class InfoPanel extends Component {
     render() {
         return (
             <Panel>
                 <Panel.Body>
-                    {this.props.content}
-                    {this.props.content ?
-                        <Button
-                            className="pull-right"
-                            bsSize="xsmall"
-                            bsStyle={"danger"}
-                            onClick={() => this.props.handleClick()}
-                        >
-                            X
-                        </Button>
-                        : null
-                    }
+                    <Grid>
+                        <Row>
+                            <Col sm={3} className={"morning"}>
+                                {/*console.log(this.props.content)*/}
+                                {this.props.content.morning} &nbsp;
+                                <img
+                                    alt={""}
+                                    src={ this.props.content.morning === ""?
+                                    require('./images/unknow.png') :
+                                    require('./images/' + this.props.content.morning + '.png')
+                                } />
+                            </Col>
+                            <Col sm={3} className={"afternoon"}>
+                                {this.props.content.afternoon} &nbsp;
+                                <img
+                                    alt={""}
+                                    src={ this.props.content.afternoon === ""?
+                                    require('./images/unknow.png') :
+                                    require('./images/' + this.props.content.afternoon + '.png')
+                                } />
+                            </Col>
+                            <Col sm={3} className={"evening"}>
+                                {this.props.content.evening} &nbsp;
+                                <img
+                                    alt={""}
+                                    src={ this.props.content.evening === ""?
+                                    require('./images/unknow.png') :
+                                    require('./images/' + this.props.content.evening + '.png')
+                                } />
+                            </Col>
+                            <Col sm={3}>
+                                {<Button
+                                    bsSize="xsmall"
+                                    bsStyle={"danger"}
+                                    onClick={() => this.props.handleClick()}
+                                >
+                                    X
+                                </Button>
+                                }
+                            </Col>
+                        </Row>
+                    </Grid>
                 </Panel.Body>
             </Panel>);
     }
 }
-
-InfoPanel.propTypes = {content: PropTypes.string};
